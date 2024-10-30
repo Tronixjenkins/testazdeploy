7fa5f3778572dd0a0cef25d4ea26e26873a75e05
stephen osei <oseibus@gmail.com>
2024-10-30 01:03:13 -0400

    testing az cd

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
    use_oidc = true
    skip_provider_registration = true
-   subscription_id = "cf14364a-3e4e-4743-a822-ee4936af4892"
+   
    features {}
    
 }
